713c9077cb47bddae8834a346590b3c912685719
unknown <bk10sandesh@gmail.com>
2023-12-06 21:55:42 +0545

    responive home page

--- Form/Form.js
+++ Form/Form.js
@@ -8,11 +8,11 @@ function createDivElement(class1, class2, listTypes) {
 const listData = [
     { class1: 'circle1', class2: 'line1', inputclass: "login" ,  listTypes: "Login / SignUp" },
     { class1: 'circle2', class2: 'line2', inputclass: "Info" , listTypes: "Basic info" },
-    { class1: 'circle3', class2: 'line3', inputclass: "trip" , listTypes: "Trip details" },
-    { class1: 'circle4', class2: 'line4', inputclass: "hotel" , listTypes: "Hotels" },
-    { class1: 'circle5', class2: 'line5', inputclass: "transport" , listTypes: "Transportation" },
-    { class1: 'circle6', class2: 'line6', inputclass: "activity" , listTypes: "Activities and interest" },
-    { class1: 'circle7', class2: 'line7', inputclass: "price" , listTypes: "Budget and preference" },
+    // { class1: 'circle3', class2: 'line3', inputclass: "trip" , listTypes: "Trip details" },
+    { class1: 'circle3', class2: 'line3', inputclass: "hotel" , listTypes: "Hotels and transportation" },
+    // { class1: 'circle5', class2: 'line5', inputclass: "transport" , listTypes: "Transportation" },
+    // { class1: 'circle6', class2: 'line6', inputclass: "activity" , listTypes: "Activities and interest" },
+    // { class1: 'circle7', class2: 'line7', inputclass: "price" , listTypes: "Budget and preference" },
     { class1: 'circle8', inputclass: "review" , listTypes: "Review and submit" },
 ];
 
